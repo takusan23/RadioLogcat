b4fe97bd2cc878b45b9a5c3aa355a9af9a62fdd2
takusan23 <takumu1200@gmail.com>
2025-09-04 19:25:12 +0900

    targetSdk / AGP / Gradle / Kotlin / Jetpack Compose のバージョンアップ

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "io.github.takusan23.radiologcat"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "io.github.takusan23.radiologcat"
         minSdk = 21
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
